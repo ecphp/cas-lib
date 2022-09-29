195d220ee0e31d1828f45e893ae090b1ef9de8da
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-09-29 13:50:46 +0200

    fix: make sure `Login` parameters are passed to the `service` url, before adding default parameters.

--- src/Handler/Login.php
+++ src/Handler/Login.php
@@ -26,11 +26,10 @@ final class Login extends Handler implements HandlerInterface
     {
         $parameters = $this->getParameters();
         $parameters += Uri::getParams($request->getUri());
-        $parameters += $this->getProperties()['protocol'][HandlerInterface::TYPE_LOGIN]['default_parameters'] ?? [];
-        $parameters = $this->formatProtocolParameters($parameters);
 
         // Add the query parameter to the service.
         // Investigate if this is really needed.
+        // We do it here before adding the default parameters.
         $parameters['service'] = (string) Uri::withParams(
             $request->getUri(),
             array_diff_key(
@@ -41,6 +40,9 @@ final class Login extends Handler implements HandlerInterface
                 ]
             )
         );
+
+        $parameters += $this->getProperties()['protocol'][HandlerInterface::TYPE_LOGIN]['default_parameters'] ?? [];
+        $parameters = $this->formatProtocolParameters($parameters);
 
         $this->validate($request, $parameters);
 
